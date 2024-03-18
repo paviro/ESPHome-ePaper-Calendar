9537beea3274a5dcc1083510ffb3295ca6b650cc
Paul-Vincent Roll <paviro@me.com>
2024-03-18 23:02:59 +0100

    Forgot part of the multi day event code...

--- home-assistant/esphome/includes/calendar/calendar_matrix.h
+++ home-assistant/esphome/includes/calendar/calendar_matrix.h
@@ -39,3 +39,48 @@ void get_calendar_matrix(int year, int month, char cal[7][7][3]) {
         }
     }
 }
+
+// Convert a date to Julian Day Number
+long date_to_jdn(int year, int month, int day) {
+    int a = (14 - month) / 12;
+    int y = year + 4800 - a;
+    int m = month + 12 * a - 3;
+    long jdn = day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    return jdn;
+}
+
+// Parse a date string "YYYY-MM-DD" and convert to integers
+void parse_date(const char *date_str, int *year, int *month, int *day) {
+    char date_copy[11]; // Buffer to hold a copy of the date string
+    strncpy(date_copy, date_str, 10); // Make a copy of the date string
+    date_copy[10] = '\0'; // Ensure null-termination
+    
+    char *token = strtok(date_copy, "-"); // Split the string by '-'
+    if (token) {
+        *year = atoi(token);
+        token = strtok(NULL, "-");
+        if (token) {
+            *month = atoi(token);
+            token = strtok(NULL, "-");
+            if (token) {
+                *day = atoi(token);
+            }
+        }
+    }
+}
+
+// Calculate the difference in days between two dates given in "YYYY-MM-DD" format
+int days_between_dates(const char *start_date, const char *end_date) {
+    int start_year, start_month, start_day;
+    int end_year, end_month, end_day;
+    
+    // Parse start date
+    parse_date(start_date, &start_year, &start_month, &start_day);
+    
+    // Parse end date
+    parse_date(end_date, &end_year, &end_month, &end_day);
+    
+    long start_jdn = date_to_jdn(start_year, start_month, start_day);
+    long end_jdn = date_to_jdn(end_year, end_month, end_day);
+    return (int)(end_jdn - start_jdn);
+}
